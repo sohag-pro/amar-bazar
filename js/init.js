68f933775a9b1d3fe937c48c18c3a2da9222d391
shuvo575 <shohaghasan575@gmail.com>
2020-04-05 20:50:38 +0600

    sale added

--- js/init.js
+++ js/init.js
@@ -1,8 +1,10 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
     $('.fixed-action-btn').floatingActionButton();
+    const forms = document.querySelectorAll('.side_add_product');
+    M.Sidenav.init(forms, { edge: 'right' });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
